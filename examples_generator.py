5bcf73667a5a302ba32760c2363ff22179f8e2f6
Lumir Balhar <lbalhar@redhat.com>
2020-04-22 15:23:10 +0200

    Some test files and modules

---
+++ examples_generator.py
@@ -0,0 +1,17 @@
+import marshal
+
+data = {
+    "integer": 42,
+    "float": 42.88,
+    "string": "hello world",
+    "list_of_simple_objects": [True, False, None, ...],
+    "set": set((9, 8, 7)),
+    "tuple": (6, 5, 4),
+    "dict": dict(a=1, b=2, c=3),
+    "complex": complex(15, 2),
+    "frozenset": frozenset((99, 999, 9999)),
+}
+
+for key, value in data.items():
+    with open(f"test/data/{key}.dat", mode="wb") as fh:
+        marshal.dump(value, fh)
---
+++ test/python/test_module.py
@@ -0,0 +1,13 @@
+def funkce():
+    a = 42
+    b = 42.8
+    return a, b
+
+
+print(funkce())
+
+a, b = funkce()
+
+(a, b)
+[a, b]
+{a: b}
